91381ea0ea858d894a714fb43ce83d2771b9f483
Shayle Senoron <107881371+seno2share@users.noreply.github.com>
2026-03-03 00:46:20 +0000

    Step 4 - added sp_delete_pokemon

--- pl.sql
+++ pl.sql
@@ -1,25 +1,18 @@
+-- Citation: https://canvas.oregonstate.edu/courses/2031764/pages/exploration-pl-slash-sql-part-2-stored-procedures-for-cud?module_item_id=26243430
+
+
 -- --------------------------------------------------------------
 -- DELETE a Pokemon from the Pokemon page
 -- --------------------------------------------------------------
 
-DROP PROCEDURE IF EXISTS sp_DeletePokemon;
+DROP PROCEDURE IF EXISTS sp_delete_pokemon;
 
 DELIMITER //
 
-CREATE PROCEDURE sp_DeletePokemon(IN p_pokemon_id INT)
+CREATE PROCEDURE sp_delete_pokemon(IN p_pokemon_id INT)
 BEGIN
-    DECLARE rows_deleted INT;
-    START TRANSACTION;
-
     DELETE FROM Pokemon
     WHERE pokemon_id = p_pokemon_id;
-
-    IF rows_deleted = 0 THEN
-        ROLLBACK;
-    ELSE
-        COMMIT;
-    END IF;
-    
 END //
 
 DELIMITER ;
